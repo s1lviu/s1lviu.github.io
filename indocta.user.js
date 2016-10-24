f05bd077851eb1cba8c6856d1ec777b7dc87411c
Silviu Bogdan Stroe <silviu@silviu-s.com>
2016-10-24 17:32:08 +0300

    Update indocta.user.js

--- indocta.user.js
+++ indocta.user.js
@@ -15,7 +15,7 @@
 //
 // @require https://code.jquery.com/jquery-2.1.1.min.js
 //
-// @version         1.0
+// @version         1.1
 // @updateURL		https://s1lviu.github.io/indocta.user.js
 //
 // @run-at			document-start|document-end
